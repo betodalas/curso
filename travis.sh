f8e8594474651ddb27c6e3bc36a0adcce2135b75
Roberto Dalas <roberto.dalas@lendico.de>
2018-05-18 16:36:35 -0300

    teste

--- travis.sh
+++ travis.sh
@@ -2,5 +2,6 @@ curl -Ls https://convox.com/install/linux.zip > /tmp/convox.zip
 sudo unzip /tmp/convox.zip -d /usr/local/bin
 convox update
 convox login console.convox.com -p $PASS
+convox switch lendico-stg/lendico-stg-ecs
 convox apps
 echo "hello"
